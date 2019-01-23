dc213e9e30f17c955a9eacdf878c362002f93fe3
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-22 21:34:19 -0500

    Update proj1_task3.1.py

--- proj1_task3.1.py
+++ proj1_task3.1.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as pt
 from proj1_task1 import *
 from proj1_task2 import closed_form
 
-
 # It a list of data points, where each datapoint is a dictionary with the following attributes:
 # popularity_score : a popularity score for this comment (based on the number of upvotes) (type: float)
 # children : the number of replies to this comment (type: int)
@@ -21,16 +20,20 @@ with open("proj1_data.json") as fp:
 #print(r)
 
 
-#for Task 3.1
+# #for Task 3.1
 [x_training, y_training] = proj1_task1.splitData(data,0,10000)
 [x_validation, y_validation] = proj1_task1.splitData(data,10001,11000)
-#[x_test, y_test] = proj1_task1.splitData(data,11001,12000)
+[x_test, y_test] = proj1_task1.splitData(data,11001,12000)
 w = []
 w = closed_form(x_training, y_training)
 y_predicted = np.matmul(w,x_validation.T)
+y_predicted2 = np.matmul(w,x_test.T)
 
 error = np.square(np.subtract(y_predicted, y_validation)).mean()
 print('The mean-squared error on the validation set is:', error)
+
+error2 = np.square(np.subtract(y_predicted2, y_test)).mean()
+print('The mean-squared error on the test set is:', error2)
 
 
 
@@ -39,7 +42,3 @@ print('The mean-squared error on the validation set is:', error)
 # print(len(x1))
 # print(len(x2))
 # print(len(x3))
-
-
-
-# if word == '[deleted]'
