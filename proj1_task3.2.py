9917108dd18febfe19f2b6b3b32344835480db4c
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-24 15:06:55 -0500

    Update proj1_task3.2.py

--- proj1_task3.2.py
+++ proj1_task3.2.py
@@ -16,25 +16,48 @@ with open("proj1_data.json") as fp:
 
 
 #for Task 3.2
-[x1,x2,x3,y_training] = proj1_task1.splitData(data,0,1000,'Task3.2')
-[x1_validation, x2_validation, x3_validation, y_validation] = proj1_task1.splitData(data,1001,1200,'Task3.2')
-[x1_test, x2_test, x3_test, y_test] = proj1_task1.splitData(data,1101,1300,'Task3.2')
+[x1,x2,x3,y_training] = proj1_task1.splitData(data,0,10000,'Task3.2')
+one1 = np.ones(len(x1))
+newx1 = np.column_stack((x1,one1))
+one2 = np.ones(len(x2))
+newx2 = np.column_stack((x2,one2))
+one3 = np.ones(len(x3))
+newx3 = np.column_stack((x3,one3))
 
+[x1_validation, x2_validation, x3_validation, y_validation] = proj1_task1.splitData(data,10001,11000,'Task3.2')
+one1_v = np.ones(len(x1_validation))
+newx1_v = np.column_stack((x1_validation,one1_v))
+one2_v = np.ones(len(x2_validation))
+newx2_v = np.column_stack((x2_validation,one2_v))
+one3_v = np.ones(len(x3_validation))
+newx3_v = np.column_stack((x3_validation,one3_v))
+
+[x1_test, x2_test, x3_test, y_test] = proj1_task1.splitData(data,11001,12000,'Task3.2')
+one1_t = np.ones(len(x1_test))
+newx1_t = np.column_stack((x1_test,one1_t))
+one2_t = np.ones(len(x2_test))
+newx2_t = np.column_stack((x2_test,one2_t))
+one3_t = np.ones(len(x3_test))
+newx3_t = np.column_stack((x3_test,one3_t))
+
+print(newx2)
+
+'''
 w1 = []
 w2 = []
 w3 = []
-w1 = closed_form(x1, y_training)
-w2 = closed_form(x2, y_training)
-w3 = closed_form(x3, y_training)
+w1 = closed_form(newx1, y_training)
+w2 = closed_form(newx2, y_training)
+w3 = closed_form(newx3, y_training)
 
-y1_predicted = np.matmul(w1,x1_validation.T)
-y1_predicted2 = np.matmul(w1,x1_test.T)
+y1_predicted = np.matmul(w1.T,x1_validation.T)
+y1_predicted2 = np.matmul(w1.T,x1_test.T)
 
-y2_predicted = np.matmul(w2,x2_validation.T)
-y2_predicted2 = np.matmul(w2,x2_test.T)
+y2_predicted = np.matmul(w2.T,x2_validation.T)
+y2_predicted2 = np.matmul(w2.T,x2_test.T)
 
-y3_predicted = np.matmul(w3,x3_validation.T)
-y3_predicted2 = np.matmul(w3,x3_test.T)
+y3_predicted = np.matmul(w3.T,x3_validation.T)
+y3_predicted2 = np.matmul(w3.T,x3_test.T)
 
 #errors for the validation set
 error1 = np.square(np.subtract(y1_predicted, y_validation)).mean()
@@ -55,3 +78,5 @@ print('The mean-squared error on the test set including the top 60 words is:', e
 
 error6 = np.square(np.subtract(y3_predicted2, y_test)).mean()
 print('The mean-squared error on the test set including the top 160 words is:', error6)
+
+'''
