df26a6488d58a021d63a014d9c817a15f946152a
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-25 12:11:24 -0500

    Update proj1_task3.1.py

--- proj1_task3.1.py
+++ proj1_task3.1.py
@@ -5,6 +5,7 @@ from proj1_task1 import splitData
 from proj1_task2 import closed_form
 from proj1_task2 import grad_des
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
 
 
 # It a list of data points, where each datapoint is a dictionary with the following attributes:
@@ -23,21 +24,24 @@ newx_training = []
 newx_validation = []
 newx_test = []
 
-[x_training, y_training] = proj1_task1.splitData(data,1001,11000,'Task3.1')
+
+#def task32 (change)
+
+
+[x_training, y_training] = splitData(data,1001,11000,'Task3.1')
 
 one1 = np.ones(len(x_training))
 newx_training = np.column_stack((x_training,one1))
 
-[x_validation, y_validation] = proj1_task1.splitData(data,11001,12000,'Task3.1')
+[x_validation, y_validation] = splitData(data,11001,12000,'Task3.1')
 
 one2 = np.ones(len(x_validation))
 newx_validation = np.column_stack((x_validation,one2))
 
-[x_test, y_test] = proj1_task1.splitData(data,0,1000,'Task3.1')
+[x_test, y_test] = splitData(data,0,1000,'Task3.1')
 
 one3 = np.ones(len(x_test))
 newx_test = np.column_stack((x_test,one3))
-
 
 
 #Closed_form approach
@@ -45,24 +49,24 @@ w = []
 w = closed_form(newx_training, y_training)
 
 
-y_predicted = np.matmul(w.T, newx_validation.T)
-y_predicted2 = np.matmul(w.T, newx_test.T)
+yclose_predicted_val = np.matmul(newx_validation,w)
+yclose_predicted_test = np.matmul(newx_test,w)
 
 
-error = np.square(np.subtract(y_predicted, y_validation)).mean()
-print('The mean-squared error on the validation set is:', error)
+close_error_val = np.square(np.subtract(yclose_predicted_val, y_validation)).mean()
+print('The mean-squared error on the validation set is:', close_error_val)
 
-error2 = np.square(np.subtract(y_predicted2, y_test)).mean()
-print('The mean-squared error on the test set is:', error2)
+close_error_test = np.square(np.subtract(yclose_predicted_test, y_test)).mean()
+print('The mean-squared error on the test set is:', close_error_test)
 
 
 
-lm2 = linear_model.LinearRegression()
+# lm2 = linear_model.LinearRegression()
 
-lm2.fit(newx_training,y_training)
-pr = lm2.predict(newx_validation)
-errorsk = np.square(np.subtract(pr, y_validation)).mean()
-print('The mean-squared error using sklearn with no text features is:', errorsk)
+# lm2.fit(newx_training,y_training)
+# pr = lm2.predict(newx_validation)
+# errorsk = np.square(np.subtract(pr, y_validation)).mean()
+#print('The mean-squared error using sklearn with no text features is:', errorsk)
 
 '''
 
@@ -72,16 +76,16 @@ w0 = np.random.random((len(newx_training[0]),1))
 beta = list(range(0,len(newx_training)))
 # print(len(w0))
 # print(len(x_training))
-wd = grad_des(newx_training,y_training,w0,beta,0.00001,0.000001,0) #X, Y, w0, beta, eta0, eps
+wd = grad_des(newx_training,y_training,w0,beta,0.00001,0.000001,0) #X, Y, w0, beta, eta0, eps, 
 
-y_predicted = np.matmul(wd.T,newx_validation.T)
+ygrad_predicted_val = np.matmul(newx_validation,wd)
 
-y_predicted2 = np.matmul(wd.T,newx_test.T)
+ygrad_predicted_test = np.matmul(newx_test,wd)
 
-error = np.square(np.subtract(y_predicted, y_validation)).mean()
-print('The mean-squared error on the validation set is:', error)
-error2 = np.square(np.subtract(y_predicted2, y_test)).mean()
-print('The mean-squared error on the test set is:', error2)
+grad_error_validation = np.square(np.subtract(ygrad_predicted_val, y_validation)).mean()
+print('The mean-squared error on the validation set is:', grad_error_validation)
+grad_error_test = np.square(np.subtract(ygrad_predicted_test, y_test)).mean()
+print('The mean-squared error on the test set is:', grad_error_test)
 
 '''
 
