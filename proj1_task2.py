9da7840e56dfcb7254df36fc879b42e2cb443819
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-25 15:09:25 -0500

    Update proj1_task2.py

--- proj1_task2.py
+++ proj1_task2.py
@@ -7,12 +7,11 @@ Rebecca Salganik
 
 import numpy as np
 
-# X : N*(m+1)
-# Y : N*1
-
 def closed_form(X, Y):
 
-    w = np.matmul(np.linalg.inv( np.matmul(X.T, X) ) ,np.matmul(X.T, Y))
+    x1 = np.matmul(X.T, X)
+    x2 = np.matmul(X.T, Y)
+    w = np.matmul(np.linalg.inv(x1) , x2)
     
     return w
 
@@ -31,27 +30,7 @@ def grad_des(X, Y, w0, beta, eta0, eps, r):
         alfa = eta0/(1+beta[i])
         w = w_old - 2*alfa * (1.0/m) * (np.matmul(X.T, np.matmul(X, w_old)) - np.matmul(X.T,Y) - r*w_old)
         d = np.linalg.norm( w - w_old )
-        w_old = w
-
-        #print(w_old)
-        
+        w_old = w        
         i += 1
 
     return w
-
-
-# for testing
-
-# X = np.array([[1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1]] , dtype=np.float64)
-# Y = np.array([[1.5], [2]  , [3], [4], [3.5], [3.8]], dtype=np.float64)
-
-
-
-# w0 = np.random.random((len(X[0]),1))
-# beta = list(range(0,400))
-# beta = [x / 300 for x in beta]
-# beta = [0 for x in beta]
-# w = closed_form(X, Y)
-# print(w)
-# w = grad_des(X, Y, w0, beta, 0.1, 0.00001)
-# print(w)
