37b39b56e4a6b396d5e27441db0759978c7ecc63
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-25 15:08:16 -0500

    Update proj1_task3.1.py

--- proj1_task3.1.py
+++ proj1_task3.1.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as pt
 from proj1_task1 import splitData
 from proj1_task2 import closed_form
 from proj1_task2 import grad_des
-from sklearn import linear_model
-from sklearn.metrics import mean_squared_error
-
 
 # It a list of data points, where each datapoint is a dictionary with the following attributes:
 # popularity_score : a popularity score for this comment (based on the number of upvotes) (type: float)
@@ -20,75 +17,51 @@ with open("proj1_data.json") as fp:
 
 
 #for Task 3.1
-newx_training = []
-newx_validation = []
-newx_test = []
+x_training = []
+x_validation = []
+x_test = []
+
+def task31(x): #add bias
+	one = np.ones(len(x))
+	newx = np.column_stack((x,one))
+	return newx
+
+def error_print(y_val,y_tes):
+	error_validation = np.square(np.subtract(y_val, y_validation)).mean()
+	error_test = np.square(np.subtract(y_tes, y_test)).mean()
+	print('The mean-squared error on the validation set is:', error_validation)
+	print('The mean-squared error on the test set is:', error_test)
 
 
-#def task32 (change)
+[x_tr, y_training] = splitData(data,0,10000,'Task3.1')
+x_training = task31(x_tr)
+
+[x_v, y_validation] = splitData(data,10000,11000,'Task3.1')
+x_validation = task31(x_v)
+
+[x_te, y_test] = splitData(data,11000,12000,'Task3.1')
+x_test = task31(x_te)
 
 
-[x_training, y_training] = splitData(data,1001,11000,'Task3.1')
-
-one1 = np.ones(len(x_training))
-newx_training = np.column_stack((x_training,one1))
-
-[x_validation, y_validation] = splitData(data,11001,12000,'Task3.1')
-
-one2 = np.ones(len(x_validation))
-newx_validation = np.column_stack((x_validation,one2))
-
-[x_test, y_test] = splitData(data,0,1000,'Task3.1')
-
-one3 = np.ones(len(x_test))
-newx_test = np.column_stack((x_test,one3))
-
+'''
 
 #Closed_form approach
 w = []
-w = closed_form(newx_training, y_training)
+w = closed_form(x_training, y_training)
+yclosed_predicted_val = np.matmul(x_validation,w)
+yclosed_predicted_test = np.matmul(x_test,w)
 
-
-yclose_predicted_val = np.matmul(newx_validation,w)
-yclose_predicted_test = np.matmul(newx_test,w)
-
-
-close_error_val = np.square(np.subtract(yclose_predicted_val, y_validation)).mean()
-print('The mean-squared error on the validation set is:', close_error_val)
-
-close_error_test = np.square(np.subtract(yclose_predicted_test, y_test)).mean()
-print('The mean-squared error on the test set is:', close_error_test)
-
-
-
-# lm2 = linear_model.LinearRegression()
-
-# lm2.fit(newx_training,y_training)
-# pr = lm2.predict(newx_validation)
-# errorsk = np.square(np.subtract(pr, y_validation)).mean()
-#print('The mean-squared error using sklearn with no text features is:', errorsk)
+error_print(yclosed_predicted_val, yclosed_predicted_test)
 
 '''
 
 #Gradient descent approach
 wd = []
-w0 = np.random.random((len(newx_training[0]),1))
-beta = list(range(0,len(newx_training)))
-# print(len(w0))
-# print(len(x_training))
-wd = grad_des(newx_training,y_training,w0,beta,0.00001,0.000001,0) #X, Y, w0, beta, eta0, eps, 
+w0 = np.random.random((len(x_training[0]),1))
+beta = list(range(0,len(x_training)))
+wd = grad_des(x_training,y_training,w0,beta,0.00001,0.000001,0) #X, Y, w0, beta, eta0, eps, r
 
-ygrad_predicted_val = np.matmul(newx_validation,wd)
+ygrad_predicted_val = np.matmul(x_validation,wd)
+ygrad_predicted_test = np.matmul(x_test,wd)
 
-ygrad_predicted_test = np.matmul(newx_test,wd)
-
-grad_error_validation = np.square(np.subtract(ygrad_predicted_val, y_validation)).mean()
-print('The mean-squared error on the validation set is:', grad_error_validation)
-grad_error_test = np.square(np.subtract(ygrad_predicted_test, y_test)).mean()
-print('The mean-squared error on the test set is:', grad_error_test)
-
-'''
-
-
-
-
+error_print(ygrad_predicted_val,ygrad_predicted_test)
