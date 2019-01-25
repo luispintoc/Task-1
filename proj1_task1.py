63b737c80aac3950f57ecb82c396404b4af79990
Mahyar_Bayran <mahyar.bayran@mail.mcgill.ca>
2019-01-25 10:16:25 -0500

    half fixed

--- proj1_task1.py
+++ proj1_task1.py
@@ -11,166 +11,183 @@ import matplotlib.pyplot as pt
 
 class proj1_task1:
 
-	def splitData(data,first_datapoint,last_datapoint,TaskNumber): 
-	#for training (0,10000) - for validation (10001,11000) and testing (11001,12000)
-		i = first_datapoint
-		
-		## VARAIBLES ##
-		is_root_list = []
-		popularity_list = []
-		controversiality_list = []
-		children_list = []
-		comments_list = []
-		words = []
-		sentence = []
-		extLinks = [] 
+	def splitData(data,first_datapoint,last_datapoint,TaskNumber):
+                #for training (0,10000) - for validation (10001,11000) and testing (11001,12000)
+                i = first_datapoint
+                
+                ## VARAIBLES ##
+                is_root_list = []
+                popularity_list = []
+                controversiality_list = []
+                children_list = []
+                comments_list = [] 
+                words = []
+                sentence = []
+                extLinks = [] 
 
-		def bool_to_binary(feature):
-			if feature is False:
-				return 0
-			else: 
-				return 1
+                def bool_to_binary(feature):
+                        if feature is False:
+                                return 0
+                        else: 
+                                return 1
 
-		dict = {}
+                dict = {}
 
-		def putInDict(text_list):
-		    for text in text_list: 
-		        newWord = filterOutPunc(text)
-		        if newWord in dict: 
-		            counter = dict[newWord] +1
-		            dict[newWord] = counter 
-		        else: 
-		            #newWord = filterOutPunc(text)
-		            dict[newWord] = 1
+                def putInDict(text_list):
+                    for text in text_list: 
+                        newWord = text
+                        if newWord in dict: 
+                            counter = dict[newWord] +1
+                            dict[newWord] = counter 
+                        else: 
+                            #newWord = filterOutPunc(text)
+                            dict[newWord] = 1
 
-		def filterOutPunc(text): 
-		    endCheck = len(text)-1
-		    tempWord = text
-		    if(text[endCheck] == '!' or text[endCheck] == '.' or text[endCheck] == '?'): 
-		        tempWord = text[0:endCheck]
-		        #print("the word is:" + tempWord)
-		    if(text[0] == '"' ):
-		        #print("found a quote--front")
-		        tempWord = text[1:]
-		        #print(tempWord)
-		    if(text[endCheck] == '"' ):
-		        #print("found a quote--back")
-		        tempWord = text[:endCheck]
-		        #print(tempWord)
-		    if(text[0] == '"' and text[endCheck] == '"'):
-		        #print("single word in quotes")
-		        tempWord = text[1:endCheck]
-		    if(text[endCheck] == ',' ):
-		        #print("found a comma")
-		        tempWord = text[:endCheck]
-		    if(text[0] == '*' ):
-		        #print("found an asterisk--front")
-		        tempWord = text[1:]
-		    if(text[endCheck] == '*' ):
-		        #print("found an asterisk-back")
-		        tempWord = text[:endCheck]
-		    if(text[0] == '*' and text[endCheck] == '*'):
-		        #print("single word in asterisks")
-		        tempWord = text[1:endCheck]
-		    
-		    return tempWord       
+                def filterOutPunc(text): 
+                    endCheck = len(text)-1
+                    tempWord = text
+                    if(text[endCheck] == '!' or text[endCheck] == '.' or text[endCheck] == '?'): 
+                        tempWord = text[0:endCheck]
+                        #print("the word is:" + tempWord)
+                    if(text[0] == '"' ):
+                        #print("found a quote--front")
+                        tempWord = text[1:]
+                        #print(tempWord)
+                    if(text[endCheck] == '"' ):
+                        #print("found a quote--back")
+                        tempWord = text[:endCheck]
+                        #print(tempWord)
+                    if(text[0] == '"' and text[endCheck] == '"'):
+                        #print("single word in quotes")
+                        tempWord = text[1:endCheck]
+                    if(text[endCheck] == ',' ):
+                        #print("found a comma")
+                        tempWord = text[:endCheck]
+                    if(text[0] == '*' ):
+                        #print("found an asterisk--front")
+                        tempWord = text[1:]
+                    if(text[endCheck] == '*' ):
+                        #print("found an asterisk-back")
+                        tempWord = text[:endCheck]
+                    if(text[0] == '*' and text[endCheck] == '*'):
+                        #print("single word in asterisks")
+                        tempWord = text[1:endCheck]
+                    
+                    return tempWord       
 
-		def hasExternalLink(text_list):
-			extLinkCount = 0
-			
-			for text in text_list: 
-				if (text[0:2] == "www"):
-					extLinkCount += 1
-				
-			return(extLinkCount)
+                def hasExternalLink(text_list):
+                        extLinkCount = 0
+                        
+                        for text in text_list: 
+                                if (text[0:2] == "www"):
+                                        extLinkCount += 1
+                                
+                        return(extLinkCount)
 
+                
+                while i < last_datapoint:
+                        
+                        is_root = bool_to_binary(data[i]['is_root'])
+                        is_root_list.append(is_root)
+                        
+                        popularity_list.append(data[i]['popularity_score'])
 
-		while i < last_datapoint:
-			
-			is_root = bool_to_binary(data[i]['is_root'])
-			is_root_list.append(is_root)
-			
-			popularity_list.append(data[i]['popularity_score'])
+                        cm = data[i]['text']
+                        cm.encode('ascii', 'ignore')
+                        comments_list.append(cm)
 
-			comments_list.append(data[i]['text'])
+                        controversiality_list.append(data[i]['controversiality'])
 
-			controversiality_list.append(data[i]['controversiality'])
+                        children_list.append(data[i]['children'])
 
-			children_list.append(data[i]['children'])
-
-			text_list = []
-			text_list.append(data[i]['text'].lower().split())
-			sentence.append(text_list[0][0])
-			putInDict(sentence)
-			extLinks.append(hasExternalLink(sentence))
-			
-			i += 1
-
-
-		def topNwords(dict,N):
-		    sorted_d = sorted(dict.items(), key=lambda x:x[1])
-		    i = 0
-		    topwords = []
-		    while i < N:
-		        topwords.append(sorted_d[len(sorted_d)-1-i])
-		        i += 1
-		    return topwords
+                        text_list = []
+                        text_list.append(data[i]['text'].lower().split())
+                        sentence.append(text_list[0][0])
+                        putInDict(sentence)
+                        extLinks.append(hasExternalLink(sentence))
+                        
+                        i += 1
 
 
-		def dictToMatrix (popList, text_data): #first input: N top words , second input: comments
-		    X = []
-		    X = np.zeros( (len(text_data), len(popList)) )
-		    row = 0
-		    column = 0
-		    for sentence in text_data: 
-		        column = 0
-		        for entry in popList: 
-		            counter = 0
-		            for word in sentence:
-		                if word == entry:
-		                    counter += 1
-		                
-		            X[row, column] = counter
-		            column += 1
-		        row += 1
-		    return X
+                def topNwords(dict,N):
+                        sorted_d = sorted(dict.items(), key=lambda x:x[1])
+                        i = 0
+                        topwords = []
+                        while i < N:
+                                topwords.append(sorted_d[len(sorted_d)-1-i])
+                                i += 1
+                        
+                        return topwords
+
+                
+                def dictToMatrix (popList, text_data): #first input: N top words , second input: comments
+                    X = np.zeros( (len(text_data), len(popList)) )
+                    row = 0
+                    column = 0
+                    for sentence in text_data: 
+                        column = 0
+                        for entry in popList: 
+                            counter = 0
+                            for word in sentence:
+                                if word == entry:
+                                    counter += 1
+                                
+                            X[row, column] = counter
+                            
+                            column += 1
+                        row += 1
+                    return X
 
 
-		#To see the top words
-		#r = topNwords(dict,160)
-		#return (x,y,r)
+                #To see the top words
+                #r = topNwords(dict,160)
+                #return (x,y,r)
 
-		y = []
-		y = np.zeros((len(popularity_list),1))
-		row = 0
+                y = []
+                y = np.zeros((len(popularity_list),1))
+                row = 0
 
-		for word in popularity_list:
-			y[row,0] = word
-			row += 1
+                for word in popularity_list:
+                        y[row,0] = word
+                        row += 1
 
-		if TaskNumber == 'Task3.1':		
-		
-			#Use this X for Task 3.1
-			x = np.column_stack((children_list,controversiality_list,is_root_list))
-			return (x, y)		
+                if TaskNumber == 'Task3.1':		
+                
+                        #Use this X for Task 3.1
+                        x = np.column_stack((children_list,controversiality_list,is_root_list))
+                        return (x, y)		
 
-		
-		if TaskNumber == 'Task3.2':
-			#Use this x for Task 3.2
-			
-			top60_words = dictToMatrix(topNwords(dict,60),comments_list)
-			top160_words = dictToMatrix(topNwords(dict,160),comments_list)
+                
+                if TaskNumber == 'Task3.2':
+                        #Use this x for Task 3.2
+                        tmp = topNwords(dict,60)
+                        top60_words = []
+                        for item in tmp:
+                                top60_words.append(item[0])
 
-			x_no_text = np.column_stack((children_list,controversiality_list,is_root_list))
-			x_top_60 = np.column_stack((children_list,controversiality_list,is_root_list,top60_words))
-			x_top_160 = np.column_stack((children_list,controversiality_list,is_root_list,top160_words))
-			return (x_no_text, x_top_60, x_top_160, y)
-		
-		if TaskNumber == 'Task3.3':
-			#Use this for x for Task 3.3
-		
-			x = np.column_stack((children_list,controversiality_list,is_root_list,extLinks))
-			return(x, y)
+                        tmp = topNwords(dict,160)
+                        top160_words = []
+                        for item in tmp:
+                                top160_words.append(item[0])
+                                
+                                
+                        top60_words_counts = dictToMatrix(top60_words,comments_list)
+                        top160_words_counts = dictToMatrix(top160_words,comments_list)
+
+                        print(sum(top60_words_counts))
+                        #print(topNwords(dict,60))
+                        #print(comments_list[0][1])
+                        
+                        x_no_text = np.column_stack((children_list,controversiality_list,is_root_list))
+                        x_top_60 = np.column_stack((children_list,controversiality_list,is_root_list,top60_words_counts))
+                        x_top_160 = np.column_stack((children_list,controversiality_list,is_root_list,top160_words_counts))
+                        return (x_no_text, x_top_60, x_top_160, y)
+                
+                if TaskNumber == 'Task3.3':
+                        #Use this for x for Task 3.3
+                
+                        x = np.column_stack((children_list,controversiality_list,is_root_list,extLinks))
+                        return(x, y)
 
 
 
--- proj1_task3.2.py
+++ proj1_task3.2.py
@@ -15,68 +15,52 @@ with open("proj1_data.json") as fp:
     data = json.load(fp)
 
 
+def task32(x_tr, y_tr, x_v, y_v):
+    # add bias
+    one1 = np.ones(len(x_tr))
+    x_tr = np.column_stack((x_tr, one1))
+    one1 = np.ones(len(x_v))
+    x_v = np.column_stack((x_v, one1))
+
+    print('Number of columns: ',len(x_tr[0]))
+    print('Rank of x_tr: ', np.linalg.matrix_rank(x_tr))
+
+    # solve
+    w = closed_form(x_tr, y_tr)
+    y_predicted = np.matmul(w.T, x_v.T)
+    #errors for the validation set
+    error = np.square(np.subtract(y_predicted, y_v)).mean()
+    print('The MSE on the validation set: ', error)
+
+    
 #for Task 3.2
-[x1,x2,x3,y_training] = proj1_task1.splitData(data,0,10000,'Task3.2')
-one1 = np.ones(len(x1))
-newx1 = np.column_stack((x1,one1))
-one2 = np.ones(len(x2))
-newx2 = np.column_stack((x2,one2))
-one3 = np.ones(len(x3))
-newx3 = np.column_stack((x3,one3))
-
-[x1_validation, x2_validation, x3_validation, y_validation] = proj1_task1.splitData(data,10001,11000,'Task3.2')
-one1_v = np.ones(len(x1_validation))
-newx1_v = np.column_stack((x1_validation,one1_v))
-one2_v = np.ones(len(x2_validation))
-newx2_v = np.column_stack((x2_validation,one2_v))
-one3_v = np.ones(len(x3_validation))
-newx3_v = np.column_stack((x3_validation,one3_v))
-
+[x1_tr, x2_tr, x3_tr, y_tr] = proj1_task1.splitData(data,0,10000,'Task3.2')
+[x1_v, x2_v, x3_v, y_v] = proj1_task1.splitData(data,10001,11000,'Task3.2')
 [x1_test, x2_test, x3_test, y_test] = proj1_task1.splitData(data,11001,12000,'Task3.2')
-one1_t = np.ones(len(x1_test))
-newx1_t = np.column_stack((x1_test,one1_t))
-one2_t = np.ones(len(x2_test))
-newx2_t = np.column_stack((x2_test,one2_t))
-one3_t = np.ones(len(x3_test))
-newx3_t = np.column_stack((x3_test,one3_t))
 
-print(newx2)
 
+#task32(x1_tr, y_tr, x1_v, y_v)
+task32(x2_tr, y_tr, x2_v, y_v)
+#task32(x3_tr, y_tr, x3_v, y_v)
 '''
-w1 = []
-w2 = []
-w3 = []
-w1 = closed_form(newx1, y_training)
-w2 = closed_form(newx2, y_training)
-w3 = closed_form(newx3, y_training)
-
-y1_predicted = np.matmul(w1.T,x1_validation.T)
-y1_predicted2 = np.matmul(w1.T,x1_test.T)
-
-y2_predicted = np.matmul(w2.T,x2_validation.T)
-y2_predicted2 = np.matmul(w2.T,x2_test.T)
-
-y3_predicted = np.matmul(w3.T,x3_validation.T)
-y3_predicted2 = np.matmul(w3.T,x3_test.T)
-
-#errors for the validation set
-error1 = np.square(np.subtract(y1_predicted, y_validation)).mean()
-print('The mean-squared error on the validation set with no text features is:', error1)
-
-error2 = np.square(np.subtract(y2_predicted, y_validation)).mean()
-print('The mean-squared error on the validation set including the top 60 words is:', error2)
-
-error3 = np.square(np.subtract(y3_predicted, y_validation)).mean()
-print('The mean-squared error on the validation set including the top 160 words is:', error3)
-
-#errors for the test set
-error4 = np.square(np.subtract(y1_predicted2, y_test)).mean()
-print('The mean-squared error on the test set with no text features is:', error4)
-
-error5 = np.square(np.subtract(y2_predicted2, y_test)).mean()
-print('The mean-squared error on the test set including the top 60 words is:', error5)
-
-error6 = np.square(np.subtract(y3_predicted2, y_test)).mean()
-print('The mean-squared error on the test set including the top 160 words is:', error6)
-
+for j in range(4,64):
+    c = 0
+    for i in range(0, len(x1_tr)):
+        c += x2_tr[i][j]
+    print(c)
+    
 '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
