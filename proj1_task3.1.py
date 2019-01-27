f6f55cfee5ad627bfd1129419e44ca9a35ad5f16
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-27 13:49:30 -0500

    Update proj1_task3.1.py

--- proj1_task3.1.py
+++ proj1_task3.1.py
@@ -46,19 +46,28 @@ x_validation = task31(x_v)
 x_test = task31(x_te)
 
 '''
-
 #Closed_form approach
+time_closed_list = []
+
 start1 = time.time()
 w = []
 w = closed_form(x_training, y_training)
 yclosed_predicted_training = np.matmul(x_training,w)
 yclosed_predicted_val = np.matmul(x_validation,w)
 
+print('Closed form approach:')
 error_print(yclosed_predicted_training, yclosed_predicted_val)
 
 end1 = time.time()
 closed_time = end1-start1
+time_closed_list.append(closed_time)
 
+# i = 0
+# while i < 20:
+# 	i += 1
+# avg_time = sum(time_closed_list)/len(time_closed_list)
+
+# print('The closed-form runtime is: ', avg_time)
 '''
 
 
@@ -105,52 +114,65 @@ while i < 1000:
 	i += 1
 
 #Plots
+
+'''
 #code to test different initial learning rates
 
 pt.figure(1)
-pt.scatter(time_list,eta0_list,s=15,color='b')
+pt.scatter(time_list,eta0_list,s=10,color='b')
+one2 = np.ones(len(eta0_list))*0.0000486
+one3 = np.ones(len(eta0_list))*0.0000505
+pt.plot(time_list,one2,'k-',color='k')
+pt.plot(time_list,one3,'k-',color='k')
 axes = pt.gca()
 axes.set_ylim([min(eta0_list),max(eta0_list)])
+axes.set_xlim([0,2.25])
 pt.ticklabel_format(style='sci',axis='y',scilimits=(-7,-5))
-pt.xlabel('time[s]')
-pt.ylabel('eta0')
-pt.title('Runtime for different learning rates')
+pt.xlabel('time [s]')
+pt.ylabel('${\eta_0}$')
+pt.title('Runtime for different initial learning rates')
 #print(time_list)
 #pt.show()
 
 pt.figure(2)
-pt.scatter(eta0_list,yt_list,s=15,color='b')
+pt.scatter(eta0_list,yt_list,s=10,color='b')
+pt.plot(one2,yt_list,'k-',color='k')
+pt.plot(one3, yt_list,'k-',color='k')
 axes = pt.gca()
 axes.set_xlim([min(eta0_list),max(eta0_list)])
 axes.set_ylim([min(yt_list),max(yt_list)])
 pt.ticklabel_format(style='sci',axis='x',scilimits=(-7,-5))
 pt.ylabel('Error')
-pt.xlabel('eta0')
-pt.title('Mean-squared error on the training set as a function of eta0')
+pt.xlabel('${\eta_0}$')
+pt.title('Mean-squared error on the training set as a function of ${\eta_0}$')
 
 pt.figure(3)
-pt.scatter(eta0_list,yv_list,s=15,color='b')
+pt.scatter(eta0_list,yv_list,s=10,color='b')
+pt.plot(one2,yv_list,'k-',color='k')
+pt.plot(one3, yv_list,'k-',color='k')
 axes = pt.gca()
 axes.set_xlim([min(eta0_list),max(eta0_list)])
 axes.set_ylim([min(yv_list),max(yv_list)])
 pt.ticklabel_format(style='sci',axis='x',scilimits=(-7,-5))
-pt.xlabel('eta0')
+pt.xlabel('${\eta_0}$')
 pt.ylabel('Error')
-pt.title('Mean-squared error on the validation set as a function of eta')
+pt.title('Mean-squared error on the validation set as a function of ${\eta_0}$')
 pt.show()
 
-
 '''
+
 #code to test stepsize in beta (speed of the decay)
 
 
 pt.figure(1)
-pt.scatter(time_list,step_size_list,s=15,color='b')
+pt.scatter(time_list,step_size_list,s=15,color='k')
+one2 = np.ones(len(time_list))*0.00000001
+pt.plot(time_list,one2,'k-',color='r')
 axes = pt.gca()
 axes.set_ylim([min(step_size_list),max(step_size_list)])
 #pt.ticklabel_format(style='sci',axis='y',scilimits=(-7,-5))
 pt.xlabel('time[s]')
-pt.ylabel('step size in beta')
+pt.ylabel('step size in ${\beta}$')
 pt.title('Runtime for different decay rates')
 
 
@@ -162,7 +184,7 @@ axes.set_ylim([min(yt_list),max(yt_list)])
 pt.ticklabel_format(style='sci',axis='x',scilimits=(-7,-5))
 pt.ylabel('Error')
 pt.xlabel('Step Size')
-pt.title('Mean-squared error on the training set as a function of beta step size')
+pt.title('Mean-squared error on the training set as a function of ${\beta}$ step size')
 
 pt.figure(3)
 pt.scatter(step_size_list,yv_list,s=15,color='b')
@@ -172,7 +194,5 @@ axes.set_ylim([min(yv_list),max(yv_list)])
 pt.ticklabel_format(style='sci',axis='x',scilimits=(-7,-5))
 pt.xlabel('Step_size')
 pt.ylabel('Error')
-pt.title('Mean-squared error on the validation set as a function of beta step size')
+pt.title('Mean-squared error on the validation set as a function of ${\beta}$ step size')
 pt.show()
-
-'''
