bece6d6b7b43434dca9bc8056a79d51e65d1eb00
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-29 12:02:18 -0500

    Update proj1_task3.2.py

--- proj1_task3.2.py
+++ proj1_task3.2.py
@@ -3,17 +3,7 @@ import numpy as np
 import matplotlib.pyplot as pt
 from proj1_task1 import splitData
 from proj1_task2 import closed_form
-from proj1_task2 import grad_des
-import warnings
-
-#warnings.filterwarnings("ignore")
-
-# It a list of data points, where each datapoint is a dictionary with the following attributes:
-# popularity_score : a popularity score for this comment (based on the number of upvotes) (type: float)
-# children : the number of replies to this comment (type: int)
-# text : the text of this comment (type: string)
-# controversiality : a score for how "controversial" this comment is (automatically computed by Reddit)
-# is_root : if True, then this comment is a direct reply to a post; if False, this is a direct reply to another comment 
+import time
 
 with open("proj1_data.json") as fp:
     data = json.load(fp)
@@ -24,13 +14,11 @@ def bias(x):
 	newx = np.column_stack((x,one))
 	return newx
 
-def error_print(y_train,y_val,y_tes):
+def error_print(y_train,y_val,):
 	error_training = np.square(np.subtract(y_train,y_training)).mean()
 	error_validation = np.square(np.subtract(y_val, y_validation)).mean()
-	error_test = np.square(np.subtract(y_tes, y_test)).mean()
 	print('The mean-squared error on the training set is:', error_training)
 	print('The mean-squared error on the validation set is:', error_validation)
-	print('The mean-squared error on the test set is:', error_test)
 
 
 #for Task 3.2
@@ -44,46 +32,26 @@ x1validation = bias(x1_v)
 x2validation = bias(x2_v)
 x3validation = bias(x3_v)
 
-[x1_te, x2_te, x3_te, y_test] = splitData(data,11000,12000,'Task3.2')
-x1test = bias(x1_te)
-x2test = bias(x2_te)
-x3test = bias(x3_te)
 
-def task32(xt,xv,xtest,y):
+def task32(xt,xv,y):
 	w = closed_form(xt,y)
 	y_predicted_train = np.matmul(xt,w)
 	y_predicted_val = np.matmul(xv,w)
-	y_predicted_test = np.matmul(xtest,w)
-	error_print(y_predicted_train,y_predicted_val,y_predicted_test)
+	error_print(y_predicted_train,y_predicted_val)
 
-print('Closed form:')
+print('Task 3.2: Linear regression using closed-form approach')
+start = time.time()
 print('Errors for set with no text features:')
-task32(x1training,x1validation,x1test,y_training)
+task32(x1training,x1validation,y_training)
+end = time.time()
+print('Time elapsed using no text features: ', end-start)
+start = time.time()
 print('Errors for set with top 60 words:')
-task32(x2training,x2validation,x2test,y_training)
+task32(x2training,x2validation,y_training)
+end = time.time()
+print('Time elapsed using top 60 words: ', end-start)
+start = time.time()
 print('Errors for set with top 160 words:')
-task32(x3training,x3validation,x3test,y_training)
-
-
-'''
-
-
-#Gradient descent approach
-def task321(xt,xv,xtest,y):
-	w0 = np.random.random((len(xt[0]),1))+1
-	beta = np.linspace(0,0.4,500)
-	w = grad_des(xt,y,w0,beta,0.47,0.001,39)
-	y_predicted_train = np.matmul(xt,w)
-	y_predicted_val = np.matmul(xv,w)
-	y_predicted_test = np.matmul(xtest,w)
-	error_print(y_predicted_train,y_predicted_val,y_predicted_test)
-
-print('Gradient descent:')
-print('Errors for set with no text features:')
-task321(x1training,x1validation,x1test,y_training)
-print('Errors for set with top 60 words:')
-task321(x2training,x2validation,x2test,y_training)
-print('Errors for set with top 160 words:')
-task321(x3training,x3validation,x3test,y_training)
-
-'''
+task32(x3training,x3validation,y_training)
+end = time.time()
+print('Time elapsed using top 160 words: ', end-start)
