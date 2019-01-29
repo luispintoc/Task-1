e21235887187805ed182c4e477ec37cdfe720aa2
luispintoc <46802439+luispintoc@users.noreply.github.com>
2019-01-28 19:42:27 -0500

    Update proj1_task3.3.py

--- proj1_task3.3.py
+++ proj1_task3.3.py
@@ -1,19 +1,65 @@
-import json
+import json # we need to use the JSON package to load the data, since the data is stored in JSON format
 import numpy as np
 import matplotlib.pyplot as pt
-from proj1_task1 import *
+from proj1_task1 import splitData
 from proj1_task2 import closed_form
+from proj1_task2 import grad_des
+import warnings
+
+#warnings.filterwarnings("ignore")
+
+# It a list of data points, where each datapoint is a dictionary with the following attributes:
+# popularity_score : a popularity score for this comment (based on the number of upvotes) (type: float)
+# children : the number of replies to this comment (type: int)
+# text : the text of this comment (type: string)
+# controversiality : a score for how "controversial" this comment is (automatically computed by Reddit)
+# is_root : if True, then this comment is a direct reply to a post; if False, this is a direct reply to another comment 
 
 with open("proj1_data.json") as fp:
     data = json.load(fp)
 
-w4 = [] 
 
-[x4, y_training] = proj1_task1.splitData(data,0,1000,'Task3.3')
-[x4_validation, y_validation] = proj1_task1.splitData(data,1001,1200,'Task3.3')
-[x4_test, y_test] = proj1_task1.splitData(data,1101,1300,'Task3.3')
-y4_predicted = np.matmul(w4,x4_validation.T)
+def bias(x):
+	one = np.ones(len(x))
+	newx = np.column_stack((x,one))
+	return newx
+
+def error_print(y_train,y_val,y_tes):
+	error_training = np.square(np.subtract(y_train,y_training)).mean()
+	error_validation = np.square(np.subtract(y_val, y_validation)).mean()
+	error_test = np.square(np.subtract(y_tes, y_test)).mean()
+	print('The mean-squared error on the training set is:', error_training)
+	print('The mean-squared error on the validation set is:', error_validation)
+	print('The mean-squared error on the test set is:', error_test)
 
 
-#errors 
-error4 = np.square(np.subtract(y4_predicted, y_validation)).mean()
+#for Task 3.3
+[x1_t,x2_t,x3_t,y_training] = splitData(data,0,10000,'Task3.3')
+x1training = bias(x1_t)
+x2training = bias(x2_t)
+x3training = bias(x3_t)
+
+[x1_v, x2_v, x3_v, y_validation] = splitData(data,10000,11000,'Task3.3')
+x1validation = bias(x1_v)
+x2validation = bias(x2_v)
+x3validation = bias(x3_v)
+
+[x1_te, x2_te, x3_te, y_test] = splitData(data,11000,12000,'Task3.3')
+x1test = bias(x1_te)
+x2test = bias(x2_te)
+x3test = bias(x3_te)
+
+def task33(xt,xv,xtest,y):
+	w = closed_form(xt,y)
+	y_predicted_train = np.matmul(xt,w)
+	y_predicted_val = np.matmul(xv,w)
+	y_predicted_test = np.matmul(xtest,w)
+	error_print(y_predicted_train,y_predicted_val,y_predicted_test)
+
+print('Closed form:')
+print('Errors for set with no text features:')
+task33(x1training,x1validation,x1test,y_training)
+print('Errors for set with top 60 words:')
+task33(x2training,x2validation,x2test,y_training)
+print('Errors for set with top 160 words:')
+task33(x3training,x3validation,x3test,y_training)
